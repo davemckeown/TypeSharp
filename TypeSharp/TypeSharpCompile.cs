8b2ab03ca3812beb45c84512aefe3f221a5afdba
Dave McKeown <davemckeown@outlook.com>
2013-10-18 19:48:29 -0400

    Reorg/Rename files, simplify TypeSharp attribute

---
+++ TypeSharp/TypeSharpCompile.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TypeSharpCompile.cs" company="TypeSharp Project">
+//     Apache 2.0 License
+// </copyright>
+// <author>Dave McKeown</author>
+
+namespace TypeSharp
+{
+    using System;
+
+    /// <summary>
+    /// TypeSharpIgnore marker attribute for compile-able constructors, methods and properties
+    /// </summary>
+    [System.AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor, AllowMultiple = false)]
+    public class TypeSharpCompile : System.Attribute
+    {
+    }
+}
